8d1a89a32ce8badc3b9b0fe2c499b8c2fbb5f62f
Gingertonic <bethmschofield@gmail.com>
2019-12-03 18:44:44 +0000

    extracting chords from each section

--- src/containers/Section.js
+++ src/containers/Section.js
@@ -4,11 +4,19 @@ import { chords } from '../testItems/newWorld'
 
 const Section = ({ content }) => {
 
-    const renderContent = () => content.map((line, i) => <Line key={i} line={line} />)
+    const renderContent = content.map((line, i) => <Line key={i} line={line} />)
+    const renderLineChords = () => content.map(line => renderChords(line))
+    const renderChords = line => line.map(syll => syll.type === "chord" ? console.log(syll.body) : null)
+
 
     return (
         <div className="section">
-            { renderContent() }
+            <div id="section-content">
+                { renderContent }
+            </div>
+            <div id="section-chords">
+                { renderLineChords() }
+            </div>      
         </div>
     )        
 
