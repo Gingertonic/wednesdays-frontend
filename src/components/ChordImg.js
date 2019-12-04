7a7514196cf4c8d6818acfe124f153835dc8765d
Gingertonic <bethmschofield@gmail.com>
2019-12-04 00:58:48 +0000

    add chord layout name tags

--- src/components/ChordImg.js
+++ src/components/ChordImg.js
@@ -4,8 +4,11 @@ import { chords } from '../testItems/newWorld'
 const ChordImg = ({ chordSymbol }) => {
     console.log(chords[chordSymbol])
     return (
+        <aside id="chord-layouts">
+            <h3 className="chord-layout-name">{chordSymbol}</h3>
+            <img className="chord-img" src={chords[chordSymbol].imgUrl} alt={chordSymbol}/>
+        </aside>
         
-        <img className="chord-img" src={chords[chordSymbol].imgUrl} alt={chordSymbol}/>
     )   
 }
 
