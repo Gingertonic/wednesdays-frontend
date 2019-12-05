9d5f4b4f2672546f669d8c9d123d1f22caac5855
Gingertonic <bethmschofield@gmail.com>
2019-12-05 12:27:48 +0000

    fix section overshoot bug

--- src/reducers/songReducer.js
+++ src/reducers/songReducer.js
@@ -20,10 +20,15 @@ export default function songReducer( state = {
                     newSectionId = state.currentSectionId - 1
                     break
                 default: newSectionId = 0
-            }          
-            const newSectionContent = state.currentSong.sections[state.currentSong.structure[newSectionId]]
-            return { ...state, currentSectionId: newSectionId, currentSectionContent: newSectionContent }
-
+            }
+            debugger
+            if ((newSectionId >= 0) && (newSectionId < state.currentSong.structure.length)){
+                const newSectionContent = state.currentSong.sections[state.currentSong.structure[newSectionId]]
+                return { ...state, currentSectionId: newSectionId, currentSectionContent: newSectionContent }
+            } else {
+                return state
+            }       
+            
         case "ADD_SONG_CHART":
             const nextSong = action.songChart
             // const nextSong = state.allSongs.find(s => s.id === parseInt(action.songId))
